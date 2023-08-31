9247d8b8d000dc05396c3c5423742c2cff4cc7a8
Suyeon Kim <ksy11090@naver.com>
2023-08-31 09:39:06 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,25 @@
 import pymongo
 import certifi
 
+"""
+효율적으로 바꾸기
+1. As a general rule, databases are slower than files.
+==> 몽고디비에서 데이터 베이스 처음에 읽어오고  csv 로 저장해서 앞으로의 코딩에서 그 파일 하나만 쓰기 ???
+
+2.  try to sort on _id as well or hint the _id index to be used
+do the task in smaller batches fetching x first Ids and moving them to a another process to continue with the next batch
+(아직 이해 완벽히 안됨)
+https://www.mongodb.com/community/forums/t/how-to-speed-up-find-projection-id-true-on-a-large-collection/124514
+
+3. To load the data efficiently, you would need to split the file and process them in parallel.
+There are two options of parallelism. You can use multi-threading or multi-processing.
+You might be using sequential processing or multi-threading and hence the delay. 
+Since the processing is done in single core by spawning multiple threads.
+But in reality you need multi-processing, that utilizes all of the 16 cores in your server.
+in python you have separate commands for doing something multi-threading vs multi-processing
+https://www.mongodb.com/community/forums/t/what-is-the-fastest-and-most-accurate-way-to-import-data/123986
+"""
+
 client = pymongo.MongoClient("mongodb+srv://kk1109kk1109:sYFQfX5J28YD71Ls@cluster0.5pusjkc.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 db = client["Cluster0"]
 
@@ -97,7 +116,9 @@ ice_max = db.item.find({"category": "제빙기",
 for doc in ice_max:
     print("ice_max", doc["name"], doc["price"])
 
-ice_min = db.item.find({"category": "제빙기", "price": {"$gte":bud*plan_price["제빙기"][0]/100}}).sort("price", pymongo.ASCENDING).limit(1)
+ice_min = db.item.find({"category": "제빙기", 
+    "price": {"$gte":bud*plan_price["제빙기"][0]/100},
+    "weight": {"$gte":bud*plan_weight["제빙기"][0]/100}}).sort("price", pymongo.ASCENDING).limit(1)
 
 for doc in ice_min:
     print("ice_min", doc["name"], doc["price"])
@@ -116,7 +137,7 @@ fridge_max = db.item.find({"category": "냉장고",
     "weight": {"$lte":bud*plan_weight["냉장고"][1]/100}}).sort("price", pymongo.DESCENDING).limit(1)
 
 for doc in fridge_max:
-    print("fridge_max", doc["name"], doc["price"])
+    print("fridge_max", doc["name"], doc["price"], doc["weight"])
 
 fridge_min = db.item.find({"category": "냉장고", 
     "price": {"$gte":bud*plan_price["냉장고"][0]/100},
@@ -155,5 +176,3 @@ blender_median = db.item.find({"category": "블렌더",
 
 for doc in blender_median:
     print("blender_median", doc["name"], doc["price"], doc["weight"])
-
-
